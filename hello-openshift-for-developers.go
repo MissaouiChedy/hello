a9ae8957ab60540dfe0b59c409bb109fd6d3b6df
Chedy Missaoui <missaoui.chedy@gmail.com>
2021-11-13 11:45:41 +0100

    Coucou

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Developers! KABLAM Edition"
+		response = "Hoho! KABLAM Edition"
 	}
 
 	fmt.Fprintln(w, response)
